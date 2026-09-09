c438d5a66b668066b1664f690bf5859d4b32703a
Gungun Agrawal <120359846+Gungunag1@users.noreply.github.com>
2026-09-09 16:27:41 +0530

    Refactor Settings class configuration
    Updated Settings class to use SettingsConfigDict for configuration.

--- app/config.py
+++ app/config.py
@@ -1,17 +1,39 @@
-from pydantic_settings import BaseSettings
+# from pydantic_settings import BaseSettings
+
+
+# class Settings(BaseSettings):
+
+#     DATABASE_URL: str
+#     OLLAMA_URL: str = "http://localhost:11434"
+#     OLLAMA_MODEL: str = "llama3.2"
+
+#     UPLOAD_DIR: str = "data/uploads"
+#     VECTOR_STORE_PATH: str = "data/faiss_index"
+
+#     class Config:
+#         env_file = ".env"
+
+
+# settings = Settings()
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
 
     DATABASE_URL: str
+
     OLLAMA_URL: str = "http://localhost:11434"
     OLLAMA_MODEL: str = "llama3.2"
 
     UPLOAD_DIR: str = "data/uploads"
     VECTOR_STORE_PATH: str = "data/faiss_index"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 
 settings = Settings()
+
